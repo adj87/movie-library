cb0521c8d7f0989f4c30a3e98776f3598f545d18
alberto jauregui dominguez <ajauregui@wtelecom.es>
2021-10-17 10:41:49 +0200

    add one more breakpoint in responsive grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    extend:{
+      screens:{
+        xs:"380px"
+      }
+    },
     colors: {
       grey: colors.coolGray,
       primary: {
