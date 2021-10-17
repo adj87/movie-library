3dfde47c0a6e58bcf04d77fc28046ffceaf5ca46
alberto jauregui dominguez <ajauregui@wtelecom.es>
2021-10-17 21:12:09 +0200

    add sticky position to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 interface HeaderProps {
-    text:string
+  text: string;
 }
 
 const Header: React.FC<HeaderProps> = ({ text }) => (
-  <div className="flex justify-items-center items-center mb-8">
+  <div className="flex justify-items-center items-center mb-8 sticky top-0 bg-primary z-10">
     <span className="flex-none mr-8">M</span>
-    <p className="font-bold flex-1 xs:text-center text-left text-3xl xs:text-5xl">{text}</p>
+    <p className="font-bold flex-1 xs:text-center text-left text-3xl xs:text-5xl">
+      {text}
+    </p>
   </div>
 );
 
