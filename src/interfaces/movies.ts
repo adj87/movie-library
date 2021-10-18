7aec747d73b2aa0467180dc0722794b8691bf2af
alberto jauregui dominguez <ajauregui@wtelecom.es>
2021-10-18 20:29:46 +0200

    add MovieWithCompanyAndActorsAsStrings interface

--- src/interfaces/movies.ts
+++ src/interfaces/movies.ts
@@ -1,11 +1,15 @@
-
 export interface Movie {
-    id: number;
-    title: string;
-    poster: string;
-    genre: string[];
-    year: number;
-    duration: number;
-    imdbRating: number;
-    actors: number[];
+  id: number;
+  title: string;
+  poster: string;
+  genre: string[];
+  year: number;
+  duration: number;
+  imdbRating: number;
+  actors: number[];
+}
+
+export interface MovieWithCompanyAndActorsAsStrings extends Movie {
+  actors: string[];
+  company: string;
 }
