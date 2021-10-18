24efe8f4feaf80e5f21c8393e83a45df41955571
alberto jauregui dominguez <ajauregui@wtelecom.es>
2021-10-18 10:49:55 +0200

    toggle menu works

--- src/components/AsideMenu/index.tsx
+++ src/components/AsideMenu/index.tsx
@@ -1,12 +1,22 @@
+import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 const AsideMenu: React.FC = () => {
   return (
-    <div className="w-full h-full fixed top-0 left-0 bg-grey-900 z-20 bg-opacity-10 bg-opacity-80">
-      <div className="w-60 h-full bg-primary p-4">
-        <p className="font-bold text-2xl">Movie Library</p>
+    <>
+      <div className="w-full h-0 fixed top-0 left-0 bg-secondary z-20 bg-opacity-90 layer transition-all duration-700">
+        <div className="w-0 h-full bg-primary aside-menu fixed top-0 left-0 z-20 transition-all duration-1000">
+          <span className="font-bold text-2xl">Movie Library</span>
+          <FontAwesomeIcon
+            icon={faTimesCircle}
+            className="text-white"
+            size="2x"
+            onClick={() => document.body.classList.remove("aside-menu-open")}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
