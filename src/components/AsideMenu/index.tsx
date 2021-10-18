fa5b81f280c1fbf4255902fd8ef21eb0b161f84b
alberto jauregui dominguez <ajauregui@wtelecom.es>
2021-10-18 11:25:18 +0200

    set padding to menu header

--- src/components/AsideMenu/index.tsx
+++ src/components/AsideMenu/index.tsx
@@ -7,13 +7,15 @@ const AsideMenu: React.FC = () => {
     <>
       <div className="w-full h-0 fixed top-0 left-0 bg-secondary z-20 bg-opacity-90 layer transition-all duration-500" />
       <div className="w-0 h-full bg-primary aside-menu fixed z-30 top-0 left-0 transition-all duration-500 overflow-hidden">
-        <span className="font-bold text-2xl">Movie Library</span>
-        <FontAwesomeIcon
-          icon={faTimesCircle}
-          className="text-white"
-          size="2x"
-          onClick={() => document.body.classList.remove("aside-menu-open")}
-        />
+        {/* Header */}
+        <div className="flex justify-between align-center items-center">
+          <span className="font-bold text-2xl">Movie Library</span>
+          <FontAwesomeIcon
+            icon={faTimesCircle}
+            className="text-white cursor-pointer"
+            onClick={() => document.body.classList.remove("aside-menu-open")}
+          />
+        </div>
       </div>
     </>
   );
