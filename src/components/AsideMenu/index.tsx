202b7d4b7e80ac7529f3e4aaedb195dd2afed982
alberto jauregui dominguez <ajauregui@wtelecom.es>
2021-10-18 12:01:50 +0200

    more styling to links

--- src/components/AsideMenu/index.tsx
+++ src/components/AsideMenu/index.tsx
@@ -1,16 +1,17 @@
 import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import { Link } from "wouter";
+import { Link, useLocation } from "wouter";
 
 const AsideMenu: React.FC = () => {
+  const [pathname] = useLocation();
   return (
     <>
       <div className="w-full h-0 fixed top-0 left-0 bg-secondary z-20 bg-opacity-90 layer transition-all duration-500" />
       <div className="w-0 h-full bg-primary aside-menu fixed z-30 top-0 left-0 transition-all duration-500 overflow-hidden">
         {/* Header */}
         <div className="flex justify-between align-center items-center">
-          <span className="font-bold text-2xl">Movie Library</span>
+          <span className="font-bold text-3xl">Movie Library</span>
           <FontAwesomeIcon
             icon={faTimesCircle}
             className="text-white cursor-pointer"
@@ -18,16 +19,26 @@ const AsideMenu: React.FC = () => {
           />
         </div>
         {/* Bodylinks */}
-        <div className="flex flex-col mt-12">
-          {links.map((el, i: number) => (
-            <Link
-              key={`link ${i}`}
-              href={el.href}
-              onClick={() => document.body.classList.remove("aside-menu-open")}
-            >
-              <a href="">{el.title}</a>
-            </Link>
-          ))}
+        <div className="flex flex-col mt-20">
+          {links.map((el, i: number) => {
+            const linkActive = pathname.includes(el.href)
+              ? "text-secondary  font-bold"
+              : "";
+            console.log(pathname, el.href);
+            return (
+              <Link
+                key={`link ${i}`}
+                href={el.href}
+                onClick={() =>
+                  document.body.classList.remove("aside-menu-open")
+                }
+              >
+                <a className={`py-2 text-grey-600 text-xl ${linkActive}`}>
+                  {el.title}
+                </a>
+              </Link>
+            );
+          })}
         </div>
       </div>
     </>
