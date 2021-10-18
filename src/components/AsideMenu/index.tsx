dd5f62d91634e6832695b6e5a0fffc9d126cdd62
alberto jauregui dominguez <ajauregui@wtelecom.es>
2021-10-18 11:11:42 +0200

    fix a little bug with toogle

--- src/components/AsideMenu/index.tsx
+++ src/components/AsideMenu/index.tsx
@@ -5,16 +5,15 @@ import React from "react";
 const AsideMenu: React.FC = () => {
   return (
     <>
-      <div className="w-full h-0 fixed top-0 left-0 bg-secondary z-20 bg-opacity-90 layer transition-all duration-700">
-        <div className="w-0 h-full bg-primary aside-menu fixed top-0 left-0 z-20 transition-all duration-1000">
-          <span className="font-bold text-2xl">Movie Library</span>
-          <FontAwesomeIcon
-            icon={faTimesCircle}
-            className="text-white"
-            size="2x"
-            onClick={() => document.body.classList.remove("aside-menu-open")}
-          />
-        </div>
+      <div className="w-full h-0 fixed top-0 left-0 bg-secondary z-20 bg-opacity-90 layer transition-all duration-500" />
+      <div className="w-0 h-full bg-primary aside-menu fixed z-30 top-0 left-0 transition-all duration-500 overflow-hidden">
+        <span className="font-bold text-2xl">Movie Library</span>
+        <FontAwesomeIcon
+          icon={faTimesCircle}
+          className="text-white"
+          size="2x"
+          onClick={() => document.body.classList.remove("aside-menu-open")}
+        />
       </div>
     </>
   );
