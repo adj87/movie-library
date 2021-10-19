a227c73c9dd755b034c973950ac44c03a9309e72
alberto jauregui dominguez <ajauregui@wtelecom.es>
2021-10-19 16:24:42 +0200

    create language buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faBars } from "@fortawesome/free-solid-svg-icons";
 import { useHistory, useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { i18n } from "i18next";
 
 interface HeaderProps {
   text?: string;
@@ -11,7 +12,8 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ text }) => {
   const { pathname } = useLocation();
   const history = useHistory();
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+
   const isBackButton =
     /movies\/\d+/.test(pathname) || pathname.includes("movies/new");
   return (
@@ -29,7 +31,22 @@ const Header: React.FC<HeaderProps> = ({ text }) => {
       <p className="font-bold flex-1 xs:text-center text-left text-3xl xs:text-5xl ellipsis-effect">
         {t(`${text}`)}
       </p>
+      <LanguageButton lang={"es"} i18n={i18n} />
+      <LanguageButton lang={"en"} i18n={i18n} />
     </div>
+  );
+};
+
+const LanguageButton = ({ lang, i18n }: { lang: string; i18n: i18n }) => {
+  const isActive =
+    i18n.language === lang ? "text-secondary font-bold text-xl" : "";
+  return (
+    <span
+      className={`mr-2 cursor-pointer ${isActive}`}
+      onClick={() => i18n.changeLanguage(lang)}
+    >
+      {lang.toUpperCase()}
+    </span>
   );
 };
 
