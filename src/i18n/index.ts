7261777e8d2a8181d94da476b5103ae7371702da
alberto jauregui dominguez <ajauregui@wtelecom.es>
2021-10-19 16:04:51 +0200

    add spanish translation file

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,15 +1,19 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import enJSON from "./en.json";
+import esJSON from "./es.json";
 
 const resources = {
   en: {
     translation: enJSON
+  },
+  es: {
+    translation: esJSON
   }
 };
 
 export default i18n.use(initReactI18next).init({
-  fallbackLng: "en",
+  fallbackLng: "es",
   resources,
   keySeparator: ".", // we do not use keys in form messages.welcome
   interpolation: {
