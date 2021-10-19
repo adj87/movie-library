393c73431c29126a509903967b52f67eb176e6ea
alberto jauregui dominguez <ajauregui@wtelecom.es>
2021-10-19 12:08:50 +0200

    remove unused element

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, RouteChildrenProps } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import Movies from "./pages/Movies";
