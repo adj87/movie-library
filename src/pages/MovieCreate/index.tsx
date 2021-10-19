ad6128d471b7897f7f13f7e0c7cbe35f18254fe7
alberto jauregui dominguez <ajauregui@wtelecom.es>
2021-10-19 14:01:09 +0200

    full implementation of selects

--- src/pages/MovieCreate/index.tsx
+++ src/pages/MovieCreate/index.tsx
@@ -1,34 +1,39 @@
 import React, { useCallback, useEffect, useState } from "react";
-import Select from "react-select";
+import Select, { MultiValue, SingleValue } from "react-select";
 
 import Body from "../../components/Body";
 import Header from "../../components/Header";
 import Input from "../../components/Input";
-import { useAppDispatch } from "../../store";
+import { Actor } from "../../interfaces/actor";
+import { Company } from "../../interfaces/company";
+import { RootState, useAppDispatch, useAppSelector } from "../../store";
 import { fetchAll } from "./redux/operations";
 
 const initialFormState = {
   id: undefined,
   title: undefined,
   poster: undefined,
-  genre: [],
+  genre: undefined,
   year: undefined,
   duration: undefined,
   imdbRating: undefined,
-  actors: [],
+  actors: undefined,
   company: undefined
 };
 
 const MovieCreate: React.FC<any> = () => {
   const [formState, setFormState] = useState(initialFormState);
   const dispatch = useAppDispatch();
+  const [actors, companies] = useAppSelector(({ movieCreate }: RootState) => [
+    movieCreate.actors,
+    movieCreate.companies
+  ]);
   const updateForm = useCallback((name, value) => {
     setFormState((oldState) => ({ ...oldState, [name]: value }));
   }, []);
   useEffect(() => {
     fetchAll(dispatch);
   }, []);
-
   return (
     <>
       <Header text={"Crear película"} />
@@ -47,27 +52,52 @@ const MovieCreate: React.FC<any> = () => {
             onChange={updateForm}
           />
           <div>
-            <label className="text-grey-200 mb-4">{"hallo"}</label>
-            <Select />
+            <label className="text-grey-200 mb-4">{"Genre"}</label>
+            <Select
+              classNamePrefix="react-select"
+              options={[
+                { value: "Fiction", label: "Fiction" },
+                { value: "Drama", label: "Drama" }
+              ]}
+              isMulti
+              value={formState.genre}
+              name="genre"
+              onChange={(genres: any, { name }) => {
+                updateForm(name, genres);
+              }}
+            />
           </div>
-          {/*           <Input
-            value={formState.genre}
-            name={"genre"}
-            label={"Genres"}
-            onChange={(name, value) => console.log(name, value)}
-          />
-          <Input
-            value={formState.actors}
-            name={"actors"}
-            label={"Actores"}
-            onChange={(name, value) => console.log(name, value)}
-          /> */}
-          <Input
-            value={formState.company}
-            name={"company"}
-            label={"Company"}
-            onChange={updateForm}
-          />
+
+          <div>
+            <label className="text-grey-200 mb-4">{"Actors"}</label>
+            <Select
+              classNamePrefix="react-select"
+              options={actors}
+              isMulti
+              getOptionValue={(opt: any) => opt.id}
+              value={formState.actors}
+              name="actors"
+              onChange={(actors: MultiValue<Actor>, { name }) => {
+                updateForm(name, actors);
+              }}
+              getOptionLabel={(el: Actor) => `${el.first_name} ${el.last_name}`}
+            />
+          </div>
+          <div>
+            <label className="text-grey-200 mb-4">{"Company"}</label>
+            <Select
+              classNamePrefix="react-select"
+              options={companies}
+              getOptionValue={(opt: any) => opt.id}
+              value={formState.company}
+              name="company"
+              onChange={(c: SingleValue<Company>, { name }) => {
+                updateForm(name, c);
+              }}
+              getOptionLabel={(c: Company) => c.name}
+            />
+          </div>
+
           <Input
             value={formState.year}
             name={"year"}
